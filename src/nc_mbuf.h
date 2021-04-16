5433fa7646977a7fd61ad930d00cd885e15aa033
Bastien Penavayre <bastienPenava@gmail.com>
2021-04-16 13:44:04 +0200

    Replace arbitraty MBUF_MIN_NUM with 1

--- src/nc_mbuf.h
+++ src/nc_mbuf.h
@@ -38,7 +38,7 @@ STAILQ_HEAD(mhdr, mbuf);
 #define MBUF_MAX_SIZE   16777216
 #define MBUF_SIZE       16384
 #define MBUF_HSIZE      sizeof(struct mbuf)
-#define MBUF_MIN_NUM    524288
+#define MBUF_MIN_NUM    1
 #define MBUF_MAX_NUM    134217728
 
 static inline bool
